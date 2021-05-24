cedc474cd7676eba5f4c1b71777f771a0af971ff
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-05-24 12:10:28 +0100

    First commit

---
+++ init.el
@@ -0,0 +1,16 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+(tool-bar-mode -1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(find-file "~/.emacs.d/startup.txt")

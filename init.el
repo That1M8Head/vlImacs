381019a380a3e53a75e66654ca4d903dcfedb1e0
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-06-03 16:10:54 +0100

    Add MELPA

--- init.el
+++ init.el
@@ -1,4 +1,10 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
 (set-face-attribute 'default nil :height 125)
 (if window-system
     (load-theme 'dracula t))
@@ -10,7 +16,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages '(gnu-elpa-keyring-update company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,3 +25,4 @@
  ;; If there is more than one, they won't work right.
  )
 (view-file "~/.emacs.d/startup.txt")
+(cd "~")

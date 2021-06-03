e7368a92f2194a45ff01e62bf1e1c7571bcbdcf4
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-06-03 16:19:57 +0100

    Change theme to Arc Dark

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ![Preview](https://github.com/That1M8Head/.emacs.d/raw/main/emacs-screenie.png)
 ========================================
 ### Stuff inside this config
-+ [Dracula theme](https://draculatheme.com/emacs)
++ [Arc Dark theme](https://github.com/cfraz89/arc-dark-theme)
 + [Custom text file](https://github.com/That1M8Head/.emacs.d/blob/main/startup.txt) [loaded on startup](https://github.com/That1M8Head/.emacs.d/blob/main/init.el#L16)
 + `tool-bar-mode`, `scroll-bar-mode` and `menu-bar-mode` disabled
--- init.el
+++ init.el
@@ -1,13 +1,7 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
 (set-face-attribute 'default nil :height 125)
 (if window-system
-    (load-theme 'dracula t))
+    (load-theme 'arc-dark t))
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)

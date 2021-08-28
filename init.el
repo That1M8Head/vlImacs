ff01b36b94de14b91ea02d183190e91496cbf2a6
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-08-28 14:47:57 +0100

    Change some things, do things, I don't know how to describe this

--- init.el
+++ init.el
@@ -1,7 +1,5 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (set-face-attribute 'default nil :height 125)
-(if window-system
-    (load-theme 'arc-dark t))
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
@@ -17,6 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:background "SlateBlue3" :foreground "white smoke" :box nil)))))
+(global-set-key (kbd "<escape>") 'keyboard-quit)
 (view-file "~/.emacs.d/startup.txt")
 (cd "~")

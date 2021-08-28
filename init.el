95300fe1325307efcfeb097f632aa8bcaecb2a9c
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-08-28 15:04:24 +0100

    Edit init.el a lot

--- init.el
+++ init.el
@@ -1,21 +1,26 @@
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+;; MELPA
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Disable unneeded stuff
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (fringe-mode -1)
+
+;; Added by Custom
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
  '(package-selected-packages '(gnu-elpa-keyring-update company)))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(mode-line ((t (:background "SlateBlue3" :foreground "white smoke" :box nil)))))
+
+;; Set ESC to C-g
 (global-set-key (kbd "<escape>") 'keyboard-quit)
+
+;; Load startup file
 (view-file "~/.emacs.d/startup.txt")
+
+;; Change to home directory
 (cd "~")

70cea0124fb7ffcfff174decd19e23a389be7d0b
Arsalan Kazmi <KAZ18001436@student.westherts.ac.uk>
2021-09-29 15:37:24 +0100

    Update init.el I think

--- init.el
+++ init.el
@@ -11,7 +11,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "SlateBlue3" :foreground "white smoke" :box nil)))))
+ '(mode-line ((t (:background "black" :foreground "white" :box nil)))))
+
+;; Colours
+(set-background-color "black")
+(set-foreground-color "white")
+
+;; C indentation
+(setq c-default-style "linux"
+      c-basic-offset 4)
 
 ;; MELPA
 (require 'package)

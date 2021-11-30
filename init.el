afab53cc5ad7f9861df6af0675e1730af0108087
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-11-30 12:18:16 +0000

    Change colour scheme to match my website

--- init.el
+++ init.el
@@ -5,17 +5,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(package-selected-packages '(evil gnu-elpa-keyring-update company)))
+ '(package-selected-packages '(## evil gnu-elpa-keyring-update company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "black" :foreground "white" :box nil)))))
+ '(mode-line ((t (:background "#171b25" :foreground "white" :box nil)))))
 
 ;; Colours
-(set-background-color "black")
+(set-background-color "#171b25")
 (set-foreground-color "white")
+
+;; Font
+(set-frame-font "Source Code Pro 11" nil t)
 
 ;; C indentation
 (setq c-default-style "linux"

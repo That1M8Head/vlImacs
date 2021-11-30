493910d32753a2b5eb1c087025f7bdd9b343310e
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-11-30 22:30:17 +0000

    Customisation

--- init.el
+++ init.el
@@ -4,19 +4,26 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(display-time-mail-face nil)
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(markdown-preview-mode markdown-mode magit w3m powerline org-preview-html ## evil gnu-elpa-keyring-update company)))
+   '(markdown-preview-eww markdown-preview-mode markdown-mode magit w3m powerline org-preview-html ## evil gnu-elpa-keyring-update company))
+ '(scalable-fonts-allowed t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "#1C263F" :foreground "white" :box nil)))))
+ '(mode-line ((t (:background "#1C263F" :foreground "white" :box (:line-width 5 :color "#1C263F")))))
+ '(mode-line-highlight ((t (:box nil))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "#171b25" :foreground "#aaaaaa" :box (:line-width 5 :color "#171b25") :weight light)))))
 
 ;; Colours
 (set-background-color "#171b25")
 (set-foreground-color "white")
+
+(display-time-mode 1)
 
 ;; C indentation
 (setq c-default-style "linux"
